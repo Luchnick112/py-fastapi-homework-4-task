7b2dfec743670b8906a3ec205d9922d2dbd1a51d
Luchnick112 <kharkivnick@gmail.com>
2026-07-23 04:11:00 +0300

    Solution2

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr, field_validator
+from pydantic import BaseModel, EmailStr, field_validator, Field
 
 from database import accounts_validators
 
@@ -45,7 +45,7 @@ class UserLoginResponseSchema(BaseModel):
 
 
 class UserRegistrationResponseSchema(BaseModel):
-    id: int
+    user_id: int = Field(validation_alias="id")
     email: EmailStr
 
     model_config = {
